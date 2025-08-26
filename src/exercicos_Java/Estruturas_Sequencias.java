666a64418e2f94e9e6ed9f9176658af5b36f42cd
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-08-26 20:02:13 -0300

    Estruturas sequenciais

---
+++ src/exercicos_Java/Estruturas_Sequencias.java
@@ -0,0 +1,11 @@
+package exercicos_Java;
+
+public class Estruturas_Sequencias {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("a");
+	}
+
+}
