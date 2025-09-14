a68bb45f348778a9b26fe5a46855251f6946fc52
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-09-13 23:30:12 -0300

    tentando

--- src/Estrutura_Repetitiva/Exercicios.java
+++ src/Estrutura_Repetitiva/Exercicios.java
@@ -10,20 +10,32 @@ public class Exercicios {
 		 */
 
 		Scanner sc = new Scanner(System.in);
+		/*
+		 * int X; int Y; String resp;
+		 * 
+		 * do { 
+		 * System.out.print("Digite dois numeros:"); X = sc.nextInt(); Y = sc.nextInt();
+		 * if (X != Y) { resp = (X < Y) ? "CRESCENTE" : "DECRESCENTE";
+		 * System.out.println(resp);
+		 * }
+		 * } while (X != Y);
+		 * -------------------------------------------------------------------------
+		 */
 
-		int X;
-		int Y;
-		String resp;
-
-		do {
-			System.out.print("Digite dois numeros:");
-			X = sc.nextInt();
-			Y = sc.nextInt();
-			if (X != Y) {
-				resp = (X < Y) ? "CRESCENTE" : "DECRESCENTE";
-				System.out.println(resp);
-			}
-		} while (X != Y);
+		/*
+		 * Problema "media_idades"
+		 */
+		
+		int idades = sc.nextInt();
+		int soma = 0;
+		int qtd = 0;
+		while(idades > 0) {
+			idades = sc.nextInt();
+			qtd++;
+			
+		}
+		soma = idades + idades / qtd;
+		System.out.println(soma);
 
 		sc.close();
 	}
