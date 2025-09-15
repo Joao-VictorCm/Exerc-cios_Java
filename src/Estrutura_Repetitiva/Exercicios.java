6c7aaa13983ce1b9337f88e8c038101a58a5db6a
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-09-14 23:19:44 -0300

    Iniciando problema 4

--- src/Estrutura_Repetitiva/Exercicios.java
+++ src/Estrutura_Repetitiva/Exercicios.java
@@ -13,29 +13,58 @@ public class Exercicios {
 		/*
 		 * int X; int Y; String resp;
 		 * 
-		 * do { 
-		 * System.out.print("Digite dois numeros:"); X = sc.nextInt(); Y = sc.nextInt();
-		 * if (X != Y) { resp = (X < Y) ? "CRESCENTE" : "DECRESCENTE";
-		 * System.out.println(resp);
-		 * }
-		 * } while (X != Y);
+		 * do { System.out.print("Digite dois numeros:"); X = sc.nextInt(); Y =
+		 * sc.nextInt(); if (X != Y) { resp = (X < Y) ? "CRESCENTE" : "DECRESCENTE";
+		 * System.out.println(resp); } } while (X != Y);
 		 * -------------------------------------------------------------------------
 		 */
 
 		/*
 		 * Problema "media_idades"
+		 * 
+		 * int idade, npessoas; double soma, media;
+		 * 
+		 * System.out.println("Digite as idades:"); idade = sc.nextInt();
+		 * 
+		 * if (idade < 0) { System.out.println("IMPOSSIVEL CALCULAR"); } else { soma =
+		 * 0; npessoas = 0; while (idade >= 0) { soma = soma + idade; npessoas++; idade
+		 * = sc.nextInt(); }
+		 * 
+		 * media = soma / npessoas;
+		 * 
+		 * System.out.printf("MEDIA = %.2f\n", media); }
+		 * 
+		 * -----------------------------------------------------------------------------
+		 * 
+		 * Problema "senha_fixa
+		 * 
+		 * System.out.println("Digite sua senha:"); int senha = sc.nextInt();
+		 * 
+		 * while (senha != 2312) { System.out.println("Senha invalida! Tente novamente:"
+		 * + senha); senha = sc.nextInt();
+		 * 
+		 * } System.out.println("Acesso permitido!");
+		 * 
+		 * -----------------------------------------------------------------------------
+		 * 
+		 * Problema "quadrante"
+		 * 
+		 * Q1 → X > 0 e Y > 0
+		 * Q2 → X < 0 e Y > 0
+		 * Q3 → X < 0 e Y < 0
+		 * Q4 → X > 0 e Y < 0
 		 */
-		
-		int idades = sc.nextInt();
-		int soma = 0;
-		int qtd = 0;
-		while(idades > 0) {
-			idades = sc.nextInt();
-			qtd++;
-			
+
+		int X = 2;
+		int Y = 2;
+
+		while (X) {
+			if(X > 0 && Y > 0) {
+				System.out.println("Q1");
+			}else if(X < 0 && Y > 0) {
+				System.out.println("Q2");
+			}else if()
 		}
-		soma = idades + idades / qtd;
-		System.out.println(soma);
 
 		sc.close();
 	}
